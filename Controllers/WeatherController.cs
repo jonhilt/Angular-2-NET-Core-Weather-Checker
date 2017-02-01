7934fbf00edaf0302d1ad6f7aa194afcfe9b315c
Jon Hilton <hilton.jon@gmail.com>
2017-02-01 20:01:36 +0000

    WeatherController now calls OpenWeather to retrieve the current weather.

--- Controllers/WeatherController.cs
+++ Controllers/WeatherController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace WeatherChecker.Controllers
 {
@@ -6,9 +12,50 @@ namespace WeatherChecker.Controllers
     public class WeatherController : Controller
     {
         [HttpGet("[action]/{city}")]
-        public IActionResult City(string city)
+        public async Task<IActionResult> City(string city)
         {
-            return Ok(new { Temp = "12", Summary = "Barmy", City = city });
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri("http://api.openweathermap.org");
+                    var response = await client.GetAsync($"/data/2.5/weather?q={city}&appid=f6c810b72d69b9224b8ddde39e19f6f9&units=metric");
+                    response.EnsureSuccessStatusCode();
+
+                    var stringResult = await response.Content.ReadAsStringAsync();
+                    var rawWeather = JsonConvert.DeserializeObject<OpenWeatherResponse>(stringResult);
+                    return Ok(new
+                    {
+                        Temp = rawWeather.Main.Temp,
+                        Summary = string.Join(",", rawWeather.Weather.Select(x => x.Main)),
+                        City = rawWeather.Name
+                    });
+                }
+                catch (HttpRequestException httpRequestException)
+                {
+                    return BadRequest($"Error getting weather from OpenWeather: {httpRequestException.Message}");
+                }
+            }
         }
+    }
+
+    public class OpenWeatherResponse
+    {
+        public string Name { get; set; }
+
+        public IEnumerable<WeatherDescription> Weather { get; set; }
+
+        public Main Main { get; set; }
+    }
+
+    public class WeatherDescription
+    {
+        public string Main { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Main
+    {
+        public string Temp { get; set; }
     }
 }
