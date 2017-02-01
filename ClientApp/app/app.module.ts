a0d1549692b40a7bf55f3ea34e76ed13ea9143c3
Jon Hilton <hilton.jon@gmail.com>
2017-02-01 19:47:54 +0000

    Initial commit (including first component and routing)

---
+++ ClientApp/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { UniversalModule } from 'angular2-universal';
+import { AppComponent } from './components/app/app.component'
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { HelloWorldComponent } from './components/helloworld/helloworld.component';
+
+@NgModule({
+    bootstrap: [ AppComponent ],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+        HomeComponent,
+        HelloWorldComponent
+    ],
+    imports: [
+        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+        RouterModule.forRoot([
+            { path: '', redirectTo: 'home', pathMatch: 'full' },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: 'hello', component: HelloWorldComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+    ]
+})
+export class AppModule {
+}
---
+++ ClientApp/app/components/helloworld/helloworld.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'helloworld',
+    template: require('./helloworld.component.html')
+})
+export class HelloWorldComponent {
+}
